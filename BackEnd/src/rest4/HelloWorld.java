9cf3a04a3a923f726a0803208fc1e4b5c51c59f3
John <JohnE.95@hotmail.com>
2019-04-02 11:04:41 +0200

    Fixat JAXB dependencys med test functioner för JSON

--- BackEnd/src/rest4/HelloWorld.java
+++ BackEnd/src/rest4/HelloWorld.java
@@ -9,6 +9,7 @@ import org.neo4j.driver.v1.*;
 
 
 @Path("admin")
+@Produces("application/json")
 public class HelloWorld {
 	
 	private String uri = "bolt://localhost:7687";
@@ -25,20 +26,40 @@ public class HelloWorld {
 	
 
 	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public Person hamtaPerson(@PathParam("id")int id) {
+		System.out.println(String.format("Söker efter person med id %d", id));
+		Person p = new Person(id,"John","Eriksson",1995);
+		return p;
+	}
+	
+	@GET
 	@Path("connect")
-	public String connect() {
+	
+	public Name connect() {
 	    Driver driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
 		try (Session session = driver.session()) {
-			   StatementResult result = session.run("MATCH (n)\r\n" + 
-			   		"RETURN count(n)");
+			   StatementResult result = session.run("MATCH (n:Java) RETURN n.name LIMIT 25");
 			   
-			  String s = result.next().toString();
-			   //List<Record> storeList = storeList(result);
 			   
+			   Record res = null;
+		       while (result.hasNext()) {
+		    	    res = result.next();
+		        	System.out.println(res.get(0));
+		        
+		            
+		       }
+		       Name n = new Name(res.get(0).toString());
+		       
+			   /*List<Record> storeList = storeList(result);
+			   for(Record r :storeList) {
+				   System.out.print(r.get(0));
+			   }*/
 			   
 		
 			   
-				return s;
+				return n;
 		}
 	
 
@@ -48,13 +69,13 @@ public class HelloWorld {
 
 	        List<Record> list = new ArrayList<>();
 	        while (statementResult.hasNext()) {
-
+	        	System.out.println(statementResult.next());
 	            list.add(statementResult.next());
 
 	        }
 
 	        return list;
 
-	    }
-*/
+	    }*/
+
 }
