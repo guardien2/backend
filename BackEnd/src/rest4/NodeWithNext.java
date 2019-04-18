a8396346d56e0e8062168cc4b1c37c9e0bebc3d8
John <JohnE.95@hotmail.com>
2019-04-18 13:50:16 +0200

    NodeWithNext

--- BackEnd/src/rest4/NodeWithNext.java
+++ BackEnd/src/rest4/NodeWithNext.java
@@ -8,20 +8,63 @@ import org.neo4j.driver.v1.types.Node;
 public class NodeWithNext {
 	
 	Node node;
-	List<NodeWithNext> nextList = new ArrayList<>();
+	String sourceFileName;
+	String fqn;
+	String fileName;
+	String name;
+	
+	List<NodeWithNext> dependsOn = new ArrayList<>();
 	
 	public NodeWithNext(Node n) {
 		this.node = n;
+		this.sourceFileName = n.get("sourceFileName").toString();
+		this.fqn = n.get("fqn").toString();
+		this.fileName = n.get("fileName").toString();
+		this.name = n.get("name").toString();
 	}
 	
+	
+
 	public Long getNodeID() {
 		return node.id();
 	}
 
-	public List<NodeWithNext> getNextlist() {
-		return nextList;
+	
+	public List<NodeWithNext> getDependsOn() {
+		return dependsOn;
 	}
 
 
+
+	public String getSourceFileName() {
+		return sourceFileName;
+	}
+
+	public void setSourceFileName(String sourceFileName) {
+		this.sourceFileName = sourceFileName;
+	}
+	public String getFqn() {
+		return fqn;
+	}
+
+	public void setFqn(String fqn) {
+		this.fqn = fqn;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
